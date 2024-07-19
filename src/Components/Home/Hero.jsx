b75d3870e9b0f3ffc96eaaaeb42255e171a07670
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-07-19 10:13:24 +0530

    "Updated Hero component with new SVG icon, added styles for buttons, margins, and padding, and refactored CSS classes for typography and colors."

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { ReactComponent as ArrowReact } from "../../assets/svg/ArrowRight.svg";
 
 export default function Hero() {
   return (
     <>
-      <div className=" m-16 rounded-md ">
-        <div className="w-[50%]">
+      <div className=" m-16 rounded-md flex">
+        <div className="w-[50%] ">
           <h1 className="text-7xl font-medium">
             Stay ahead of the curve with our forword-thinking{" "}
           </h1>
-          <h5 className="mt-8 text-lg text-gray-500">
+          <h5 className="mt-8 text-lg text-gray-500 pe-32">
             {" "}
             An award winning SEO agency with discplines in digital marketing,
             desing, and website development , focused on understanding you .
@@ -17,28 +18,39 @@ export default function Hero() {
           <div className="mt-16 flex items-center ">
             <buttom className="btn btn-neutral  rounded-full ps-6">
               {" "}
-              Schedule call{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6 ms-8"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                />
-              </svg>
+              Schedule call <ArrowReact className="size-6 ms-8" />
             </buttom>
             <buttom className="underline font-bold ms-20">
               View Case Study
             </buttom>
           </div>
+          <div className="mt-16">
+            <h1 className=" w-44 font-bold"> Trusted by the World's biggest brands </h1>
+            <div>
+              <div>
+
+                <span></span>
+              </div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </div>
-        <div></div>
+        <div className="w-[50%] py-10">
+          <div className="flex items-center justify-evenly mb-3 p-5">
+            <div className=" bg-gray-100 rounded-xl w-[43%] h-[250px]">
+              Image
+            </div>
+            <div className=" bg-gray-100 rounded-xl w-[43%] h-[250px]">
+              {" "}
+              230+
+            </div>
+          </div>
+          <div className=" bg-gray-100 w-[86%] m-auto rounded-xl h-[200px]">
+            {" "}
+            Drive more Traffic
+          </div>
+        </div>
       </div>
     </>
   );
