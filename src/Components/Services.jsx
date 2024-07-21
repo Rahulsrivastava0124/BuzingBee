fc3a6dd37ff401c58eb54a8e1f1163e7e3fe3106
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-07-21 13:28:43 +0530

    some changes in video icon in services section

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,6 +1,6 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 // Import Swiper styles
 import "swiper/css";
 import service2 from "../assets/image/services2.jpg";
@@ -8,13 +8,6 @@ import service1 from "../assets/image/services1.jpg";
 // import required modules
 
 const Services = () => {
-  const progressCircle = useRef(null);
-  const progressContent = useRef(null);
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    progressCircle.current.style.setProperty("--progress", 1 - progress);
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  };
-
   return (
     <>
       <div className="mx-16 mb-20">
@@ -87,7 +80,7 @@ const Services = () => {
           </div>
           <div
             className={
-              "w-[60%]  h-[350px] rounded-3xl bg-gray-50 overflow-hidden"
+              "w-[60%]  h-[350px] rounded-3xl bg-gray-50 "
             }
           >
             <Swiper
@@ -97,28 +90,38 @@ const Services = () => {
                 delay: 2500,
                 disableOnInteraction: false,
               }}
-              pagination={{
-                clickable: true,
-              }}
-              navigation={true}
-              modules={[Autoplay, Pagination, Navigation]}
-              onAutoplayTimeLeft={onAutoplayTimeLeft}
+              // pagination={{
+              //   clickable: true,
+              // }}
+              // navigation={true}
+              modules={[Autoplay]}
               className="mySwiper"
             >
               <SwiperSlide className="relative">
-                <img src={service1} alt="this copy ima"  style={{filter:" brightness(0.5)"}}/>
-                <span className="absolute top-[38%] text-7xl right-40 font-semibold text-white placeholder:">HOW WE WORK</span>
+                <img
+                  src={service1}
+                  alt="this copy ima"
+                  style={{ filter: " brightness(0.5)" }}
+                  className="rounded-3xl"
+                />
+                <span className="absolute top-[38%] text-7xl right-40 font-semibold text-white placeholder:">
+                  HOW WE WORK
+                </span>
               </SwiperSlide>
               <SwiperSlide className="relative">
-                <img src={service2} alt="this copy ima"  style={{filter:" brightness(0.5)"}}/>
-                <span className="absolute top-[38%] text-7xl right-40 font-semibold text-white placeholder:">TEAM WE WORK</span>
+                <img
+                  src={service2}
+                  alt="this copy ima"
+                  style={{ filter: " brightness(0.5)" }}
+                  className="rounded-3xl"
+                />
+                <span className="absolute top-[38%] text-7xl right-40 font-semibold text-white placeholder:">
+                  TEAM WE WORK
+                </span>
               </SwiperSlide>
-              <div className="autoplay-progress" slot="container-end">
-                <svg viewBox="0 0 48 48" ref={progressCircle}>
-                  <circle cx="24" cy="24" r="20"></circle>
-                </svg>
-                <span ref={progressContent}></span>
-              </div>
+              <button className="size-16 bg-yellow-300 rounded-full grid absolute bottom-3 right-3 overflow-visible z-10 ring-white ring-offset-base-100 ring ring-offset-1">
+                <i class="bi bi-play-fill m-auto text-5xl"></i>
+              </button>
             </Swiper>
           </div>
         </div>
