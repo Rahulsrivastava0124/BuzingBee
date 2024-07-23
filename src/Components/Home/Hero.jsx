a8fedfac3b52c5d367c74552fc057aef07579654
portfolio_project_demo <kumasourav890@gmail.com>
2024-07-23 14:00:28 +0530

    hero section animation added and click sound added

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -7,18 +7,18 @@ import CountUp from 'react-countup'
 export default function Hero() {
   return (
     <>
-      <div className=" mx-16 my-10 rounded-md flex" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="2000" >
+      <div className=" mx-16 my-10 rounded-md flex" >
         <div className="w-[50%] ">
-          <h1 className="text-7xl font-medium " >
+          <h1 className="text-7xl font-medium " data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
             <span data-aos="fade-up" data-aos-duration="1000">Stay ahead of the </span><span>curve with our</span><span> forword-thinking</span>{" "}
           </h1>
-          <h5 className="mt-8 text-lg text-gray-500 pe-32">
+          <h5 className="mt-8 text-lg text-gray-500 pe-32" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1100">
             {" "}
             An award winning SEO agency with discplines in digital marketing,
             desing, and website development , focused on understanding you .
           </h5>
 
-          <div className="mt-16 flex items-center ">
+          <div className="mt-16 flex items-center " data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1200">
             <buttom className="btn btn-neutral text-white rounded-full ps-6">
               {" "}
               Schedule call <ArrowReact className="size-6 ms-8" />
@@ -27,7 +27,7 @@ export default function Hero() {
               View Case Study
             </buttom>
           </div>
-          <div className="mt-16  flex ">
+          <div className="mt-16  flex " data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1300">
             <h1 className=" w-44 font-bold">
               {" "}
               Trusted by the World's biggest brands{" "}
@@ -46,7 +46,7 @@ export default function Hero() {
           </div>
         </div>
         <div className="w-[50%] py-10">
-          <div className="flex items-center justify-evenly mb-3 p-5">
+          <div className="flex items-center justify-evenly mb-3 p-5" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
             <div className=" bg-gray-50 rounded-tl-[100%] rounded-lg  w-[43%] h-[250px]  relative">
               {" "}
               <button className="btn btn-circle bg-black  p-10 w-10 h-10 absolute bottom-52 right-10 text-center">
@@ -73,23 +73,23 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <div className=" bg-black w-[86%] m-auto rounded-xl h-[200px] p-5 flex relative ">
+          <div className=" bg-black w-[86%] m-auto rounded-xl h-[200px] p-5 flex relative " data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1200">
             {" "}
             <div>
-              <div className=" flex items-center ">
+              <div className=" flex items-center " data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1300">
                 <hr className="w-16 font-bold me-5 mt-5" />
                 <h1 className="text-white  mt-5">
                   {" "}
                   Drive More Traffic and Sales{" "}
                 </h1>
               </div>
-              <div className="w-[60%]">
+              <div className="w-[60%]" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1300">
                 <h1 className="text-4xl text-white mt-4">
                   Drive more traffic and product sales
                 </h1>
               </div>
             </div>
-            <div className="absolute bottom-0 right-5">
+            <div className="absolute bottom-0 right-5" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1400">
               <img src={heroCart} alt="heroCart" width={250}/>{" "}
             </div>
           </div>
